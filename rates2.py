91ce511a8aa3bac4d53a3195a9b732d9399c3e0e
kovacsja <janos.kovacs.h@gmail.com>
2023-09-24 09:54:47 +0200

    added header to request

--- rates2.py
+++ rates2.py
@@ -1,7 +1,6 @@
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
-import streamlit as st
 
 # import streamlit as st
 
@@ -14,10 +13,13 @@ end_date = datetime.now().strftime("%Y-%m-%d")
 start_date = datetime.strftime(datetime.now() - timedelta(days=90), "%Y-%m-%d")
 out_curr: str = "HUF"
 rates = []
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+}
 
 for bcurr in base:
     url = f"https://api.exchangerate.host/timeseries?base={bcurr}&start_date={start_date}&end_date={end_date}&symbols={out_curr}"
-    response = requests.get(url, timeout=5)
+    response = requests.get(url, headers=headers, timeout=5)
 
     data = response.json()
 
